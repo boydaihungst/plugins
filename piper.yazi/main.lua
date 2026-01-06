9af86a60c5c8a6b69816966e93c583910b7dc1a5
boydaihungst <38396158+boydaihungst@users.noreply.github.com>
2026-01-06 11:27:59 +0700

    feat: support VFS for piper previewer that rely on external commands
    https://github.com/sxyazi/yazi/pull/3477

--- piper.yazi/main.lua
+++ piper.yazi/main.lua
@@ -7,7 +7,7 @@ local function fail(job, s) ya.preview_widget(job, ui.Text.parse(s):area(job.are
 function M:peek(job)
 	local child, err = Command("sh")
 		-- TODO: use `job.file.path` instead
-		:arg({ "-c", job.args[1], "sh", tostring(job.file.cache or job.file.url) })
+		:arg({ "-c", job.args[1], "sh", tostring(job.file.path or job.file.cache or job.file.url) })
 		:env("w", job.area.w)
 		:env("h", job.area.h)
 		:stdout(Command.PIPED)
